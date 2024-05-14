fdf98273985c115f7159f6dfdd5a96185a4b4cb4
hakimamarullah <hakimamarullah@gmail.com>
2024-05-15 03:45:32 +0700

    Pagination done

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,12 @@
-import NewsCard, { NewsArticle, Response } from "./components/NewsCard";
-import PaginationClient from "./components/PaginationClient";
-import { fetchData } from "./utils/utils";
+import dynamic from "next/dynamic";
+
+const NewsListDynamic = dynamic(() => import("./components/NewsList"));
 
 export default async function Home() {
-  const data = await fetchData<Response<NewsArticle[]>>(
-    "http://localhost:8080/news/articles"
-  );
-  const news: NewsArticle[] = data.data;
-
   return (
     <div className="min-h-screen flex justify-center items-center overflow-x-auto">
       <main className="max-w-screen-lg mx-auto py-4">
-        <div className="grid grid-cols-1 gap-4">
-          {news.map((item, idx) => (
-            <NewsCard key={idx} news={item} />
-          ))}
-        </div>
-        <div className="flex justify-center pt-4">
-          <PaginationClient totalPage={data.totalPage} initialPage={1} />
-        </div>
+        <NewsListDynamic />
       </main>
     </div>
   );
